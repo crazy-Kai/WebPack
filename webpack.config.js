60d2b5b04916a2f2956a95ac2fc7eef3c61e43b8
apple <apple@appledeMBP.lan>
2017-01-15 23:34:29 +0800

    add

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/*
+   webpack 配置文件
+ */
+var webpack = require("webpack"),
+    path = require("path"),
+    node_modules = path.resolve("./node_modules");
+console.log(webpack);
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: "./build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+        	// babel解析react
+            test: /(\.jsx)|(\.es6)$/,
+            exclude:[node_modules],
+            loader: "babel"
+        }, {
+        	//css
+        	test:/\.css$/,
+            exclude:[node_modules],
+        	loader:"style!css"
+        },{
+        	//less
+        	test:/\.less$/,
+        	loader:"style!css!less"
+        },{
+        	//sass
+        	test:/\.sass$/,
+        	loader:"style!css!sass"
+        }]
+    },
+    resolve:{
+    	alias:{},
+    	root:node_modules 
+    }
+
+}
